b7fa297ee829cf21996b6af4b033522716691d82
boss34128 <n_p_boss@outlook.com>
2020-10-21 14:53:29 +0700

    211063

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,136 @@
 <template>
-  <div id="app">
-    <!--<img alt="Vue logo" src="./assets/1.png">-->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <Game/>
-  </div>
+<div id="app" class="pernlung">
+    <div class="lyn">
+        <div class="row">
+            <div class="col-md-4 col-sm-4">
+                <h1>{{heron1}}</h1>
+                <b-progress :value="heroh1" :max="max" show-progress animated></b-progress>
+                <div class="herotitle tile">
+                    <img :src="heroi1" alt class="img-fluid image" :width="heroh1 + 'vw'" />
+                </div>
+            </div>
+            <div class="col-md-4 mt-5 col-sm-4 vs">
+                <img src="./assets/vs.png" />
+            </div>
+            <div class="col-md-4 col-sm-4">
+                <h1>{{monn1}}</h1>
+                <b-progress :value="monh1" :max="max2" show-progress animated></b-progress>
+                <div class="montitle tile">
+                    <img :src="moni1" alt class="img-fluid image" :width="monh1 + 'vw'" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12">
+            <Game @hero_n="hero_n1" @hero_h="hero_h1" @hero_i="hero_i1" @mon_n="mon_n1" @mon_h="mon_h1" @mon_i="mon_i1" @atkp="atk_p1" @atkm="atk_m1" @spep="spe_p1" @spem="spe_m1"></Game>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import Game from './components/Game'
-
+import Game from "./components/Game";
 export default {
-  name: 'App',
-  components: {
-    //HelloWorld
-    Game
-  }
-}
+    name: "App",
+    components: {
+        Game,
+    },
+    data: function () {
+        return {
+            heron1: "",
+            heroh1: 0,
+            heroi1: "",
+            monn1: "",
+            monh1: 0,
+            moni1: "",
+            attack: 0,
+        };
+    },
+
+    methods: {
+        //hero
+        hero_n1(value) {
+            this.heron1 = value;
+        },
+        hero_h1(value) {
+            this.heroh1 = value;
+            this.max = this.heroh1;
+        },
+        hero_i1(value) {
+            this.heroi1 = value;
+        },
+        //monster
+        mon_n1(value) {
+            this.monn1 = value;
+        },
+        mon_h1(value) {
+            this.monh1 = value;
+            this.max2 = this.monh1;
+        },
+        mon_i1(value) {
+            this.moni1 = value;
+        },
+        //attackhero
+        atk_p1(value) {
+            this.attack = value;
+            this.monh1 -= this.attack;
+        },
+        spe_p1(value) {
+            this.attack = value;
+            this.monh1 -= this.attack;
+        },
+        //attackmon
+        atk_m1(value) {
+            this.attack = value;
+            this.heroh1 -= this.attack;
+        },
+        spe_m1(value) {
+            this.attack = value;
+            this.heroh1 -= this.attack;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 50px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+}
+
+.pernlung {
+    background-image: url("assets/0.jpg");
+    background-repeat: on-repeat;
+    background-size: 100x 100%;
+    height: 100vh;
+}
+
+.lyn {
+    margin: auto;
+    width: 90%;
+    height: 600px;
+}
+
+.herotile {
+    float: center;
+}
+
+.montile {
+    float: bottom;
+}
+
+.tile {
+    display: inline-block;
+    margin: 5px;
+    bottom: 0;
+}
+
+.vs {
+    margin: auto;
 }
 </style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,110 +1,172 @@
 <template>
-  <div>
-    <p3>
-    {{player[randomNO(1,3)-1].name}}</p3><br>
+<div>
     <!--ได้ทั้งชื่อและ hp ออกมา-->
     <!--{{randomDamage(1,300)}} สุ่ม 1 - 300-->
-    <button @click="$start" class="button-a" id="yellow">START</button>
-    <button @click="$attack" class="button-a" id="purple">ATTACK</button>
-    <hr />
-    Name:{{aPlayer.name}}
-    HP:{{aPlayer.hp}}
-    image:{{aPlayer.image}}
-    <hr />
-    Name:{{bMonster.name}}
-    HP:{{bMonster.hp}}
-    image:{{bMonster.image}}
-  </div>
+    <button @click="$start" class="button-a" id="green">START</button>
+    <button @click="$attack" class="button-a" id="red">ATTACK</button>
+    <button @click="$special" class="button-a" id="yellow">SPECIAL ATTACK</button>
+</div>
 </template>
-
 
 <script>
 export default {
-  data: function () {
-    return {
-      //เอาชื่อออกจาก object player[1].name
-      //เอาชื่อออกจาก object player[random].name
-      aPlayer: { name: "", hp: 0, image: "" },
-      bMonster: { name: "", hp: 0, image: "" },
-      attack: 0,
-      special: 0,
-      player: [
-        { name: "Lyn", hp: 90, image: "a" },
-        { name: "Som", hp: 150, image: "b" },
-        { name: "Stamp", hp: 200, image: "c" },
-      ],
-      monster: [
-        { name: "Opor", hp: 60, image: "x" },
-        { name: "Khatar", hp: 70, image: "y" },
-        { name: "Oh", hp: 55, image: "z" },
-      ],
-    };
-  },
+    data: function () {
+        return {
+            //เอาชื่อออกจาก object player[1].name
+            //เอาชื่อออกจาก object player[random].name
 
-  methods: {
-    randomNO: function (min, max) {
-      return Math.max(Math.floor(Math.random() * max) + 1, min);
+            player: [{
+                    name: "Sailor Moon",
+                    hp: 305,
+                    image: require("../assets/1.png"),
+                },
+                {
+                    name: "Sailor Venus",
+                    hp: 304,
+                    image: require("../assets/2.png"),
+                },
+                {
+                    name: "Sailor Mars",
+                    hp: 303,
+                    image: require("../assets/3.png"),
+                },
+                {
+                    name: "Sailor Uranus",
+                    hp: 302,
+                    image: require("../assets/4.png"),
+                },
+                {
+                    name: "Sailor Chibi Moon",
+                    hp: 301,
+                    image: require("../assets/5.png"),
+                },
+            ],
+            monster: [{
+                    name: "Cure Bloom",
+                    hp: 300,
+                    image: require("../assets/6.png"),
+                },
+                {
+                    name: "Cure Egret",
+                    hp: 299,
+                    image: require("../assets/7.png"),
+                },
+                {
+                    name: "Cure Mint",
+                    hp: 298,
+                    image: require("../assets/8.png"),
+                },
+                {
+                    name: "Cure Pine",
+                    hp: 297,
+                    image: require("../assets/9.png"),
+                },
+                {
+                    name: "Cure Burry",
+                    hp: 296,
+                    image: require("../assets/10.png"),
+                },
+            ],
+        };
     },
-    $start: function () {
-      //1.random Plaryer 1-3 -->ชื่อ ค่า hp รูป ฝากไว้ใน aPlary
-      this.aPlayer = this.player[this.randomNO(1, 3) - 1];
-      this.bMonster = this.monster[this.randomNO(1, 3) - 1];
-      //2.random Monster 1-3
+
+    methods: {
+        randomNO: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        $start: function () {
+            //random
+            this.ranP = this.randomNO(1, 5) - 1;
+            this.ranM = this.randomNO(1, 5) - 1;
+            //player
+            this.nPlayer = this.player[this.ranP].name;
+            this.$emit("hero_n", this.nPlayer);
+            console.log("emit", this.nPlayer);
+            this.hPlayer = this.player[this.ranP].hp;
+            this.$emit("hero_h", this.hPlayer);
+            console.log("hp", this.hPlayer);
+            this.iPlayer = this.player[this.ranP].image;
+            this.$emit("hero_i", this.iPlayer);
+            console.log("im", this.iPlayer);
+            //monster
+            this.nMonster = this.monster[this.ranM].name;
+            console.log("emit", this.nMonster);
+            this.$emit("mon_n", this.nMonster);
+            this.hMonster = this.monster[this.ranM].hp;
+            this.$emit("mon_h", this.hMonster);
+            this.iMonster = this.monster[this.ranM].image;
+            this.$emit("mon_i", this.iMonster);
+        },
+        $attack: function () {
+            // p -> mon
+            // p <- mon
+            //p <=0 ===ทำไง  you lose
+            //mon <==0 ทำไง you win
+            //End
+            this.atk = this.randomNO(3, 10);
+            this.$emit("atkp", this.atk);
+            console.log("Attack :", this.atk);
+
+            this.atk = this.randomNO(3, 10);
+            this.$emit("atkm", this.atk);
+            console.log("Atk back", this.atk);
+        },
+        $special: function () {
+            this.spe = this.randomNO(10, 20);
+            this.$emit("spep", this.spe);
+            console.log("Special Attack :", this.spe);
+
+            this.spe = this.randomNO(10, 20);
+            this.$emit("spem", this.spe);
+            console.log("SPATK back", this.spe);
+        },
     },
-    $attack: function () {
-        // p -> mon
-        // p <- mon
-        //p <=0 ===ทำไง  you lose
-        //mon <==0 ทำไง you win
-        //End
-        this.aPlayer = this.player[this.randomNO(1, 3) - 1];
-        this.bMonster = this.monster[this.randomNO(1, 3) - 1];
-
-
-
-    },
-    $special: function () {},
-  },
 };
 </script>
 
 <style scoped>
 .b {
-  font-size: 50px;
+    font-size: 50px;
 }
 
 .button-a {
-  background-color: rgb(0, 0, 0);
-  border: none;
-  padding: 5px 5px 5px 5px;
-  text-align: center;
-  display: inline-block;
-  margin: 4px 2px;
-  font-size: 30px;
-  cursor: pointer;
-  border-radius: 10px;
-  border: 0.5px solid white;
-  width: 200px;
+    background-color: rgb(3, 42, 77);
+    border: none;
+    padding: 5px 5px 5px 5px;
+    text-align: center;
+    display: inline-block;
+    margin: 4px 2px;
+    font-size: 30px;
+    cursor: pointer;
+    border-radius: 10px;
+    border: 0.5px solid white;
+    width: 300px;
 }
+
 .button-a-font-dd {
-  background-color: white;
-  border: none;
-  color: #646363;
-  padding: 5px 5px 5px 5px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 10px;
-  border: 0.5px solid #ffffff;
-  width: 200px;
+    background-color: white;
+    border: none;
+    color: #646363;
+    padding: 5px 5px 5px 5px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 10px;
+    border: 0.5px solid #ffffff;
+    width: 200px;
+}
+
+button:hover#green {
+    color: #2bff00;
+}
+
+button:hover#red {
+    color: #ff0000;
 }
 
 button:hover#yellow {
-  color: #2bff00;
-}
-button:hover#purple {
-  color: #ff0000;
+    color: #fffb00;
 }
 </style>
